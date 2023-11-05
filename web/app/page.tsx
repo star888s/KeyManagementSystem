771c7ed7e44c86c673b0a0d5ec1126a101c2d0f2
star <star888s.oath@gmail.com>
2023-11-05 21:08:47 +0900

    add : deplay for amplify [KMS-3]

---
+++ web/app/page.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+export default function Home() {
+  return (
+    <main>
+      <div className="max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto">
+        <div className="flex flex-col">
+          <div className="-m-1.5 overflow-x-auto">
+            <div className="p-1.5 min-w-full inline-block align-middle">
+              <div className="bg-white border border-gray-200 rounded-xl shadow-sm overflow-hidden dark:bg-slate-900 dark:border-gray-700">
+                <div className="px-6 py-4 grid gap-3 md:flex md:justify-between md:items-center border-b border-gray-200 dark:border-gray-700">
+                  <div>
+                    <h2 className="text-xl font-semibold text-gray-800 dark:text-gray-200"></h2>
+                    Hello!
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+}
