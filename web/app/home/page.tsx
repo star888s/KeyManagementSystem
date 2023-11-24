943387ab3a699797aa92533fd934271190d67d75
star <star888s.oath@gmail.com>
2023-11-25 06:02:16 +0900

    temp : debug

--- web/app/home/page.tsx
+++ web/app/home/page.tsx
@@ -7,11 +7,6 @@ import React, { useEffect } from 'react';
 export default function Home() {
   const { data: session } = useSession();
   const router = useRouter();
-  useEffect(() => {
-    if (!session) {
-      router.push('/');
-    }
-  });
   return (
     <main>
       <div className='max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto'>
