4157d75438fe2e9909af20ef979bb7471e695923
star <star888s.oath@gmail.com>
2023-11-25 05:57:17 +0900

    fix : reference error [KMS-16]

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,23 +1,24 @@
 'use client';
 import { useSession } from 'next-auth/react';
 import { useRouter } from 'next/navigation';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Login } from './components/AuthButton';
 
 export default function Home() {
   const { data: session } = useSession();
   const router = useRouter();
-  if (!session) {
-    return (
-      <main className='flex h-screen justify-center items-center'>
-        <div className='text-center'>
-          <h1 className='text-4xl font-bold'>KMS</h1>
-          <Login></Login>
-        </div>
-      </main>
-    );
-  }
-  router.push('/home');
 
-  return null;
+  useEffect(() => {
+    if (session) {
+      router.push('/home');
+    }
+  });
+  return (
+    <main className='flex h-screen justify-center items-center'>
+      <div className='text-center'>
+        <h1 className='text-4xl font-bold'>KMS</h1>
+        <Login></Login>
+      </div>
+    </main>
+  );
 }
